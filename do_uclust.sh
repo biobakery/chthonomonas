9b538d8cd442ceef369dc5bd1e462124a4f21411
XCM <xochitl.morgan@otago.ac.nz>
2016-01-12 10:12:15 +1300

    documentation added.

--- do_uclust.sh
+++ do_uclust.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/sh
 
+#Note - this requires the file all.fna.gz in the same directory. This file is available as a download.
+
 gunzip all.fna.gz
 usearch8.1.1861_i86osx32 -cluster_fast all.fna -id 0.95 -centroids nr2.fasta --log usearch.log --uc results2.uc
